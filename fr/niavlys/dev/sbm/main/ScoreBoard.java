31c43804d584ad059644e4b4e796d9180de48ae3
Sylvain CROCQUEVIEILLE <niavlys.dev@gmail.com>
2025-01-19 16:54:56 +0100

    Refactor ScoreBoard logic and add reset functionality
    Refactored the `cacher` method to use a new `reset` method for cleaning up the scoreboard. Introduced a `reset` method to clear lines and update the scoreboard, enhancing reusability and maintainability. Minor improvements include consistent usage of `Bukkit` imports and adding colored text to scoreboard lines.

--- fr/niavlys/dev/sbm/main/ScoreBoard.java
+++ fr/niavlys/dev/sbm/main/ScoreBoard.java
@@ -1,5 +1,6 @@
 package fr.niavlys.dev.sbm.main;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.scoreboard.Objective;
@@ -38,17 +39,17 @@ public class ScoreBoard {
         p.setScoreboard(board);
     }
     public void cacher(){
-        p.setScoreboard(null);
+        reset();
     }
     public void maj(){
-        ScoreboardManager manager = org.bukkit.Bukkit.getScoreboardManager();
+        ScoreboardManager manager = Bukkit.getScoreboardManager();
         if (manager != null) {
             board = manager.getNewScoreboard();
             Objective objective = board.registerNewObjective("sb", "dummy", ChatColor.GOLD+"SunRealms");
             objective.setDisplaySlot(org.bukkit.scoreboard.DisplaySlot.SIDEBAR);
 
             for (Line line : lines) {
-                objective.getScore(line.getText()).setScore(line.getLine());
+                objective.getScore(line.getColor()+line.getText()).setScore(line.getLine());
             }
         }
     }
@@ -77,5 +78,10 @@ public class ScoreBoard {
     public void updateLine(int i, String text, ChatColor color){
         i = 100-i;
         updateLine(i, new Line(i, text, color));
+    }
+
+    public void reset(){
+        lines.clear();
+        maj();
     }
 }
